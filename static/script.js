be8815aafb6150f9732dac245ece02705fed3e08
n00dles <mike@digimute.com>
2018-09-10 21:54:01 +0100

    fixing up charts

--- static/script.js
+++ static/script.js
@@ -8,9 +8,32 @@ jQuery(document).ready(function($) {
             $.get({url: "/getinstance/"+id, 
                 success: function(result){
                     var obj = JSON.parse(result);
+                    var values =  obj.values.split(',');
+                    
                     $("#ibody-"+id).html(obj.current);
-                }
-            });
+                    console.log("VALUES: " + values);
+                    console.log(values.length);
+                    values.forEach(element => {
+                        console.log(element);
+                    });
+                    var ctx = document.getElementById("chart-"+id);
+                    var myChart = new Chart(ctx, {
+                        type: 'line',
+                            data: {
+                                labels: [16.00, 16.30, 17.00, 17.30,18.00, 18.30, 19.00, 19.30],
+                                datasets: [{
+                                    label: 'temp',
+                                    borderColor: 'ff0000',
+                                    data: values,
+                                    fill: false,
+                                }]
+			
+                        }
+                    }
+              
+                )
+            }
         }
-    );
+    )
+})
 });
